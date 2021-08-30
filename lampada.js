5cbeff6b390d158678e4b196de7e1453bbbb53a4
RAFAELA SANTOS DA SILVA <21193422@CFP127ALUNO.LOCAL>
2021-08-30 16:09:36 -0300

    lampada concluida

--- lampada.js
+++ lampada.js
@@ -1,16 +1,42 @@
 "use strict"
 const lampada = document.getElementById("lampada")
 
+function ligaDesliga (estadoLiga, estadoDesliga) {
+    const ligar = document.getElementById("ligar")
+    const desligar = document.getElementById("desligar")
+    ligar.disabled = estadoLiga
+    desligar.disabled = estadoDesliga
+}
+function lampadaInteira() {
+    return !lampada.src.includes("quebrada")
+}
 function ligarLampada() {
-     lampada.src = "img/ligada.jpg"
+    if (lampadaInteira()){
+        lampada.src = "img/ligada.jpg"
+        ligaDesliga(true, false)
+    }
+}
+function desligarLampada() {
+    if (lampadaInteira()) {
+        lampada.src = "img/desligada.jpg"
+       ligaDesliga(false, true)
+    }
+}
+function quebrarLampada() { 
+    lampada.src = "img/quebrada.jpg"
+    ligaDesliga(true, true)
 }
 //eventos
 document.getElementById("ligar")
 .addEventListener("click", ligarLampada)
 
-function desligarLampada() {
-    lampada.src = "img/desligada.jpg"
-}
+
 
 document.getElementById("desligar")
 .addEventListener("click", desligarLampada)
+
+
+document.getElementById("lampada").addEventListener("mouseover", ligarLampada)
+document.getElementById("lampada").addEventListener("mouseout", desligarLampada)
+document.getElementById("lampada").addEventListener("dblclick", quebrarLampada)
+
